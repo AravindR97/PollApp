b0fc660ef4deb7673a97976a3f59ed93fb238047
AravindR97 <132665408+AravindR97@users.noreply.github.com>
2024-05-12 15:09:42 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,29 @@
+welcome = "Welcome to Poll App!\n"
+
+menu = """\n1) Create new poll
+2) List open polls
+3) Vote on a poll
+4) Show poll votes
+5) Select a random winner from a poll option
+6) Exit\n
+"""
+
+choice = input("\nEnter your choice: ")
+
+while choice:
+    if choice == 1:
+        pass
+    elif choice == 2:
+        pass
+    elif choice == 3:
+        pass
+    elif choice == 4:
+        pass
+    elif choice == 5:
+        pass
+    elif choice == 6:
+        pass
+    else:
+        pass
+    choice = input("\nEnter your choice: ")
+
